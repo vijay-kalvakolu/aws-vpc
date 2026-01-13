c3dd1c1e365ebb21ff8b741dd83a8d7d41efde71
vijay2693 <85749478+vijay2693@users.noreply.github.com>
2026-01-13 20:34:52 +0100

    aws-module

---
+++ vpc-main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = var.region
+}
+# Rsources
+ # Respurce 1 : VPC
+ # Resource 2 : IGW
+ # Resource 3 : Public subnet
+ # Resource 4: Private Subnet
+ # Resource 5 : NAT Gateway
+ # Resource 6 : Public Route table
+ # Resource 7 : Public Route Table assocaite to Public Subnet
+ # Resource 8 : Private Route table
+ # Resource 9 : Private Route Table assocaite to Private Subnet
+ # Resource 10 : Elastic IP for NAT Gateway (EIP)
